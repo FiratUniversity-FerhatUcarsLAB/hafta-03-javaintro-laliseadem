74a31b235345bc1f8fde5aff639b19e55781bc36
laliseadem <240541610@firat.edu.tr>
2025-10-21 11:21:26 -0700

    UcMesaj.java is added

---
+++ Task-1-HosgeldinMesajlari/UcMesaj.java
@@ -0,0 +1,8 @@
+public class UcMesaj {
+    public static void main (String[] args){
+       // print sentences on the screen
+        System.out.println("Java'ya Hos Geldiniz");
+        System.out.println("Bilgisayar Bilimine Hos Geldiniz");
+        System.out.println("Programlama Eglencelidir");
+    }
+}
