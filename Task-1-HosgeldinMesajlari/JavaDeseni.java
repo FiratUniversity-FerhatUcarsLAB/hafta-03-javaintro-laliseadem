27451abac285c679c69d089b4e0dbfa7e2db229d
laliseadem <240541610@firat.edu.tr>
2025-10-21 11:17:32 -0700

    JavaDeseni is added

---
+++ Task-1-HosgeldinMesajlari/JavaDeseni.java
@@ -0,0 +1,9 @@
+public class JavaDeseni {
+    public static void main(String[]  args){
+        // creating a shape using the word java.
+        System.out.println("J     J");
+        System.out.println("A A   A");
+        System.out.println("V   V V");
+        System.out.println("A     A");
+    }
+}
