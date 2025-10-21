50b404a0fe1ce14ef950e040415770ae406160c6
laliseadem <240541610@firat.edu.tr>
2025-10-21 11:25:41 -0700

    ToplamaSeri.java is added

---
+++ Task-2-Matematikselislemler/ToplamaSeri.java
@@ -0,0 +1,7 @@
+public class ToplamaSeri {
+    public static void main (String[] args) {
+        // calculate the sum of numbers from 1 to 9
+        int sonuc = 1+2+3+4+5+6+7+8+9;
+        System.out.println("1+2+3+4+5+6+7+8+9 = " + sonuc);
+    }
+}
