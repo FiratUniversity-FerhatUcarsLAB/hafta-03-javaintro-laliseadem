02b9d36eaa40eb395e1f0dae0568daa6a7af7ac3
laliseadem <240541610@firat.edu.tr>
2025-10-21 11:07:39 -0700

    BesKereTekrer.java is added.

---
+++ Task-1-HosgeldinMesajlari/BesKereTekrar.java
@@ -0,0 +1,10 @@
+public class BesKereTekrar {
+    public static void main( String[]args){
+        // print the same sentence five times.
+        System.out.println("Java'ya Hos Geldiniz");
+        System.out.println("Java'ya Hos Geldiniz");
+        System.out.println("Java'ya Hos Geldiniz");
+        System.out.println("Java'ya Hos Geldiniz");
+        System.out.println("Java'ya Hos Geldiniz");
+    }
+}
