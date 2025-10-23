46694153c3f1888732b1ec9c9e159fef6f333616
laliseadem <240541610@firat.edu.tr>
2025-10-23 02:59:18 -0700

    DikdortgenHesab.java is added.

---
+++ Task-3-GeometrikHesaplamalar/DikdortgenHesap.java
@@ -0,0 +1,13 @@
+public class DikdortgenHesab {
+    public static void main(String[]args){
+        // create a variable and store the value of width and height.
+        double en = 4.5;
+        double boy = 7.9;
+        // calculate the area and circumstance of the rectangle.
+        double alan = en * boy;
+        double cevre = 2 *(en + boy);
+        // print the result on the screen.
+        System.out.println("Alan: " + alan);
+        System.out.println("Cevre: "+ cevre);
+    }
+}
