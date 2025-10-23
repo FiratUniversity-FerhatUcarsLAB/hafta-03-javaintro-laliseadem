728fb4fae0a57d4e8f2c5d7c5a79763466ab218e
laliseadem <240541610@firat.edu.tr>
2025-10-23 03:11:36 -0700

    Add IsimKarti class to display student information

---
+++ Task-4-StringDesenler/IsimKarti.java
@@ -0,0 +1,15 @@
+public class IsimKarti {
+    public static void main (String[]args){
+        String ad = "Lalise Adem";
+        String soyad = "Wado";
+        String ogrNo = "240541610";
+        String bolum = "Yazilim Muhendisligi";
+
+        System.out.println("OGRENCİ BİLGİLERİ");
+        System.out.println("------------------");
+        System.out.println("Ad          : " + ad);
+        System.out.println("Soyad       : " + soyad);
+        System.out.println("Ogrenci No  : " + ogrNo);
+        System.out.println("Bolum       : " + bolum);
+    }
+}
