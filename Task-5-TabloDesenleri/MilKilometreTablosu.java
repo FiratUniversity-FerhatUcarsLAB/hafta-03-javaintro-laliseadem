12c55bedecd556496e4ff3e4f77aa7caa9a78ba6
laliseadem <240541610@firat.edu.tr>
2025-10-23 03:13:47 -0700

    Add Mil to Kilometre conversion table program

---
+++ Task-5-TabloDesenleri/MilKilometreTablosu.java
@@ -0,0 +1,24 @@
+public class MilKilometreTablosu {
+    public static void main(String[] args) {
+        System.out.println("------------------------");
+        System.out.println("|Mil      | Kilometre |");
+        System.out.println("------------------------");
+
+        double mil1 = 1;
+        double km1 = mil1 * 1.609;
+        double mil2 = 5;
+        double km2 = mil2 * 1.609;
+        double mil3 = 10;
+        double km3 = mil3 * 1.609;
+        double mil4 = 20;
+        double km4 = mil4 * 1.609;
+        double mil5 = 50;
+        double km5 = mil5 * 1.609;
+        System.out.println("| " + mil1 + "     | " + km1 + "     |");
+        System.out.println("| " + mil2 + "     | " + km2 + "     |");
+        System.out.println("| " + mil3 + "    | " + km3 + "     |");
+        System.out.println("| " + mil4 + "    | " + km4 + "     |");
+        System.out.println("| " + mil5 + "    | " + km5 + "     |");
+        System.out.println("------------------------");
+    }
+}
